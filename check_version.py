4d9df9607f40ae225475e4033257b4acd21bd81e
Stepan Goryachev <53620809+cdtn@users.noreply.github.com>
2021-03-03 19:26:03 +0300

    Create check_version.py

---
+++ check_version.py
@@ -0,0 +1,10 @@
+import re
+import sys
+
+with open('batchflow/__init__.py', 'r') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if version != sys.argv[1]:
+    sys.exit(1)
+else:
+    sys.exit(0)
