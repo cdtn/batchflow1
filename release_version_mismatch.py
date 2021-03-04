189c54a7767c1446a1468384d45ae3a0c51d18de
Stepan Goryachev <53620809+cdtn@users.noreply.github.com>
2021-03-04 13:07:08 +0300

    Update release_version_mismatch.py

--- release_version_mismatch.py
+++ release_version_mismatch.py
@@ -5,6 +5,6 @@ with open('batchflow/__init__.py', 'r') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if version != sys.argv[1]:
-    sys.exit(0)
-else:
     sys.exit(1)
+else:
+    sys.exit(0)
